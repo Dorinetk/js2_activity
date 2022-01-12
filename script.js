b60b7190e03d99d861a9f7c26753678baf1d4405
Dorinetk <dketchiozo@gmail.com>
2022-01-12 17:53:32 -0500

    updated code

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 // takes an array as an argument so need a parameter and
 // returns 10 times the array length for 10 minutes per exercise
-function calculateWorkoutLength() {}
+function calculateWorkoutLength(arr) {
+    return arr.length * 10;
+}
 
 // takes two arguments an array and a number of exercises and
 // returns a new array generated with random exercises based on
 // the number passed into the parameter
-function generateRandomExercises() {}
+function generateRandomExercises(arr, exNum) {
+    const randomExercises = [];
+    //generates a random value out of an array ***
+    for (let i = 0; i < exNum; i++) {
+        const randomIndex = Math.floor(Math.random() * arr.length);
+        randomExercises.push(arr[randomIndex]);
+    }
+    return randomExercises;
+}
